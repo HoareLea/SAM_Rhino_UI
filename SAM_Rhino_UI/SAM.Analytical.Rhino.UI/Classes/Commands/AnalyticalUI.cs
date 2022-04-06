1f19d75fc1177bf431083e2703b0a0bbe12ec971
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-06 11:32:54 +0100

    AnalyticalUI Command Added

---
+++ SAM_Rhino_UI/SAM.Analytical.Rhino.UI/Classes/Commands/AnalyticalUI.cs
@@ -0,0 +1,40 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+using Rhino.Input;
+using SAM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Rhino.UI
+{
+    public class AnalyticalUI : global::Rhino.Commands.Command
+    {
+        public AnalyticalUI()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static AnalyticalUI Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "SAM_AnalyticalUI";
+
+        protected override global::Rhino.Commands.Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SAM", "SAM.Analytical.UI.exe");
+            if(!System.IO.File.Exists(path))
+            {
+                return global::Rhino.Commands.Result.Failure;
+            }
+
+            System.Diagnostics.Process.Start(path);
+            return global::Rhino.Commands.Result.Success;
+        }
+    }
+}
